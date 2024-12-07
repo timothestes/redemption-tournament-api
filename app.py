ba300c5494fb1b74cf7a68d9ea257b5a3f2902be
Tim Estes <33536315+timothestes@users.noreply.github.com>
2024-12-07 12:13:29 -0800

    try to connect to supabase

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 
+from client import supabase
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    response = supabase.table("countries").select("*").execute()
+
+    return str(response)
 
 
 @app.route("/about")
