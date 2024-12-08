5ecd4d0095a987ab7d02d767015ced672944a5f4
Tim Estes <33536315+timothestes@users.noreply.github.com>
2024-12-07 19:13:07 -0800

    switch to v1

--- routes/__init__.py
+++ routes/__init__.py
@@ -4,4 +4,4 @@ from .users import users_bp
 
 def register_routes(app):
     app.register_blueprint(main_bp)
-    app.register_blueprint(users_bp)
+    app.register_blueprint(users_bp, url_prefix="/v1")
